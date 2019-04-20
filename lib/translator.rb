3ba31fabe1a23db10e3a36b741b5ab3841ccec70
Qun Li Huang <qunli02@gmail.com>
2019-04-20 15:20:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,10 @@ def load_library(path)
   # code goes here
   hash = YAML.load_file(path)
   new = {"get_meaning" => {}, "get_emoticon" => {}}
-
+  hash.each do |meaning, emo|
+    new["get_meaning"][emo[1]] = meaning
+    new["get_emoticon"][meaning] = emo[]
+  end
   return new
 end
 

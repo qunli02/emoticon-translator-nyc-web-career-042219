2c77e9e90724e8aea80c71cb6df63c68769ec720
Qun Li Huang <qunli02@gmail.com>
2019-04-20 15:05:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,11 @@ require "yaml"
 def load_library(path)
   # code goes here
   hash = YAML.load_file(path)
+  print hash
   new = {"get_meaning" => {}, "get_emoticon" => {}}
+  hash.each do |meaning, emo|
+    
+  end
 end
 
 def get_japanese_emoticon
